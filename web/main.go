83a357d153e1bc35861f3c34d0f114c9a06d84b6
aboubakar ismael <aboubakarismael16@gmail.com>
2021-08-03 19:52:33 +0800

    Latest commit

--- web/main.go
+++ web/main.go
@@ -51,7 +51,7 @@ func openDB(dsn string) (*sql.DB, error)  {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn","root:13628@/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn","root:root@/snippetbox?parseTime=true", "MySQL data source name")
 	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge","Secret Key")
 	flag.Parse()
 
